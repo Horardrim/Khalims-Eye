f71a66954527d8550aadbde384d04e7e000625b3
TalRasha <524948250@qq.com>
2025-02-07 14:08:16 +0800

    init spring boot 3 project

---
+++ src/main/java/com/horadrim/eye/KhalimsEyeApplication.java
@@ -0,0 +1,13 @@
+package com.horadrim.eye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhalimsEyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhalimsEyeApplication.class, args);
+	}
+
+}
